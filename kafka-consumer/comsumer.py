2c9a87bf21c12a67021212e699ea5339e2da6b86
dipghoshraj <dipghoshraj@gmail.com>
2024-11-22 02:08:45 +0530

    feat : kafka consumer setup for image compression

---
+++ kafka-consumer/comsumer.py
@@ -0,0 +1,46 @@
+from kafka import KafkaConsumer, KafkaProducer
+import io, logging, boto3, os
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("KafkaWorker")
+
+
+KAFKA_BROKER_URL =os.getenv("KAFKA_BROKER_URL", "localhost:9092")
+KAFKA_UPLOAD_TOPIC = os.getenv("KAFKA_TOPIC", "image_uploads")
+
+print(KAFKA_BROKER_URL)
+
+# producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
+# producer.send(KAFKA_UPLOAD_TOPIC, value=bytes('image_id', 'utf-8'))
+
+compress_consumer = KafkaConsumer(
+    KAFKA_UPLOAD_TOPIC,
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_deserializer=lambda x: x.decode("utf-8"),
+    heartbeat_interval_ms=10000
+)
+
+
+def process_message(message):
+    """
+    Process the kafka message
+
+    :params: message: the kafka message
+    :return: None
+    """
+
+    try:
+        logger.error(f"Process the messaeg {message}")
+    except Exception as e:
+        logger.error(f"Failed to process image {message}: {str(e)}")
+
+
+def start_worker():
+
+    logger.info("Kafka worker started, waiting for messages...")
+    for message in compress_consumer:
+        process_message(message.value)
+
+if __name__ == "__main__":
+    start_worker()
---
+++ kafka-consumer/image-processor/images.py
@@ -0,0 +1,56 @@
+
+
+
+from PIL import Image
+import io, logging
+from minio import Minio
+
+
+
+class Images:
+
+    def __init__(self, logger ):
+
+        """
+        """
+        self.client = Minio("play.min.io",
+            access_key="minioadmin",
+            secret_key="minioadmin",
+        )
+        self.MINIO_BUCKET = 'filmy-image'
+        self.logger = logger
+
+    
+    def upload(self, imageID):
+        """
+        Upload the compress image and save metadat in DB with ACID
+
+        :param: filename: Raw image bytes, metadata: File metadata
+        :return None
+        """
+
+        try:
+            # response = s3_client.get_object(Bucket=CEPH_BUCKET, Key=image_id)
+            response = self.client.get_object(Bucket=self.MINIO_BUCKET, Key=imageID)
+            original_image = response["Body"].read()
+
+            compressed_image =  self.compress_image(original_image)
+            self.client.put_object(Bucket=self.MINIO_BUCKET, Key=imageID, Body=compressed_image)
+            self.logger.info(f"Resized image saved as: {imageID}")
+        except Exception as e:
+            self.logger.error(f"Failed to process image {imageID}: {str(e)}")       
+
+
+    def compress_image(self, image):
+        """
+        Compresses an image using lossless compression.
+
+        :param image_data: Raw image bytes.
+        :return: Compressed image bytes.
+        """
+        with Image.open(io.BytesIO(image)) as img:
+            output = io.BytesIO()
+            # Save image in the original format with optimization enabled
+            img.save(output, format=img.format, optimize=True)
+            output.seek(0)
+            return output.getvalue()
