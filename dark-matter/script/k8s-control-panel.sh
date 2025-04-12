142b00b2d956157752dd7474e09284ffc08e1b00
dipghoshraj <dipghoshraj@gmail.com>
2025-04-13 00:43:44 +0530

    fix : coping

--- dark-matter/script/k8s-control-panel.sh
+++ dark-matter/script/k8s-control-panel.sh
@@ -7,25 +7,25 @@ CONTROL_PLANE_INI="${INVENTORY_DIR}/control_panel.ini"
 SSH_KEY="$HOME/.ssh/id_rsa"
 SSH_USER="ubuntu"
 
-echo "🛠️  Preparing environment..."
-sudo apt install -y python3-pip
+# echo "🛠️  Preparing environment..."
+# sudo apt install -y python3-pip
 
-echo "📦 Installing dependencies..."
-sudo apt update && sudo apt install -y python3-pip sshpass git
-pip3 install --user ansible --break-system-packages # patch for the new debian version for root useage
-export PATH="$HOME/.local/bin:$PATH"
+# echo "📦 Installing dependencies..."
+# sudo apt update && sudo apt install -y python3-pip sshpass git
+# pip3 install --user ansible --break-system-packages # patch for the new debian version for root useage
+# export PATH="$HOME/.local/bin:$PATH"
 
-echo "📥 Cloning Kubespray..."
-git clone https://github.com/kubernetes-sigs/kubespray.git --depth 1 || true
-cd kubespray
-pip3 install -r requirements.txt --ignore-installed --break-system-packages # patch for the new debian version for root useage
+# echo "📥 Cloning Kubespray..."
+# git clone https://github.com/kubernetes-sigs/kubespray.git --depth 1 || true
+# cd kubespray
+# pip3 install -r requirements.txt --ignore-installed --break-system-packages # patch for the new debian version for root useage
 
-echo "📂 Preparing control-plane inventory..."
-cp -rfp inventory/sample inventory/${CLUSTER_NAME}
-cp ../${CONTROL_PLANE_INI} inventory/${CLUSTER_NAME}/inventory.ini
+# echo "📂 Preparing control-plane inventory..."
+# cp -rfp inventory/sample inventory/${CLUSTER_NAME}
+# cp ../${CONTROL_PLANE_INI} inventory/${CLUSTER_NAME}/inventory.ini
 
 echo "🚀 Deploying control-plane nodes..."
-ansible-playbook -i inventory/${CLUSTER_NAME}/inventory.ini \
+ansible-playbook -i ${CONTROL_PLANE_INI} \
   --private-key=${SSH_KEY} \
   -u ${SSH_USER} \
   cluster.yml -b -v
