e2535b6f00fb0a9be933fc9e9957b85eecdbd4b2
dipghoshraj <dipghoshraj@gmail.com>
2025-04-14 23:13:41 +0530

    fix : update the master and node generation script

--- dark-matter/script/k3s-install.sh
+++ dark-matter/script/k3s-install.sh
@@ -4,7 +4,8 @@ set -e
 echo "Installing k3s on master..."
 
 # Install k3s without Traefik or extra stuff
-curl -sfL https://get.k3s.io | sh -s - --disable traefik --disable servicelb
+curl -sfL https://get.k3s.io | sh -s - --disable traefik --disable servicelb --node-external-ip=${MASTER_IP} --tls-san=${MASTER_IP} --tls-san=${MASTER_HOSTNAME}
+
 
 echo "✅ k3s master installed"
 echo "Token (save this for worker nodes):"
