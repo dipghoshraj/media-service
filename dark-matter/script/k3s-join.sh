f6202dd4626cc547a76daf2b2bb66c7644e223f5
dipghoshraj <dipghoshraj@gmail.com>
2025-04-16 22:54:35 +0530

    feat : update the traefik config

--- dark-matter/script/k3s-join.sh
+++ dark-matter/script/k3s-join.sh
@@ -11,5 +11,5 @@ fi
 NODE_TOKEN=$1
 
 echo "Installing k3s agent on worker..."
-sudo curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_PRIVATE_IP}:6443 K3S_TOKEN=${NODE_TOKEN} sh -
+sudo curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_IP}:6443 K3S_TOKEN=${NODE_TOKEN} sh -
 echo "✅ k3s agent installed and connected to master"
