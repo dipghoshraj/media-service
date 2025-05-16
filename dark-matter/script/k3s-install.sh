b5048a1400288c24ee03bd80bfd2de5c89d18ce4
dipghoshraj <dipghoshraj@gmail.com>
2025-05-16 15:09:56 +0530

    feat : fix the install

--- dark-matter/script/k3s-install.sh
+++ dark-matter/script/k3s-install.sh
@@ -4,7 +4,7 @@ set -e
 echo "Installing k3s on master..."
 
 # Install k3s without Traefik or extra stuff
-curl -sfL https://get.k3s.io | sh -s - --disable traefik --disable servicelb --node-external-ip=${MASTER_IP} --tls-san=${MASTER_IP} --tls-san=${MASTER_HOSTNAME}
+curl -sfL https://get.k3s.io | sh -s - --disable traefik --disable servicelb --node-external-ip=15.207.221.101 --tls-san=15.207.221.101 --tls-san=ip-172-31-2-61
 
 
 echo "✅ k3s master installed"
