bbf8234637c7123158237e7e71545867732d9ed0
dipghoshraj <dipghoshraj@gmail.com>
2025-08-01 22:48:29 +0530

    feat : added the client config

--- agnitunnel/agnicli/internal/connects/client.go
+++ agnitunnel/agnicli/internal/connects/client.go
@@ -2,8 +2,10 @@ package connects
 
 import (
 	"agni-cli/internal/types"
+	"fmt"
 	"net"
 	"sync"
+	"time"
 
 	tunnelv1 "agni-cli/proto"
 )
@@ -18,3 +20,16 @@ type TunnelClient struct {
 	mu      sync.Mutex
 	Streams map[string]net.Conn // Map of stream IDs to net.Conn
 }
+
+func generateNonce() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
+}
+
+func NewTunnelClient(cfg ClientConfig) (*TunnelClient, error) {
+	return &TunnelClient{
+		Cfg:   cfg,
+		Close: make(chan struct{}),
+	}, nil
+}
+
+// log.Printf("Agent [%s] write error: %v", sess.AppID, err)
