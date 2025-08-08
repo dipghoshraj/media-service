aa8dbec214232b82e2e19119da10d6fc62f234ed
dipghoshraj <dipghoshraj@gmail.com>
2025-08-09 02:49:24 +0530

    remove the whole stack

--- indraNet-tunnels/agent-tunnel/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"agent-tunnel/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
--- indraNet-tunnels/gateway-tunnel/internal/client/auth.go
+++
@@ -1,8 +0,0 @@
-package client
-
-import "log"
-
-func verifyToken(token, secret string) bool {
-	log.Printf("Verifying token: %s with secret: %s", token, secret)
-	return true
-}
